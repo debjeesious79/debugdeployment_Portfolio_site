d235c659c4a9ea754adcf7e3a6c76c74d16cd416
debjeesious79 <nathdebjeet79@gmail.com>
2024-03-18 12:27:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer.', 'Problem Solver.', 'Web Designer.', 'Video Editer.'],
+    typeSpeed: 50,
+});
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
